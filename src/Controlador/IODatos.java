ac995e2eff467e356eb9810cca1117381a8f72bb
Kacper <alum22kborylo01@gmail.com>
2021-04-14 14:24:41 +0200

    Falta un detalle Minimo

--- src/Controlador/IODatos.java
+++ src/Controlador/IODatos.java
@@ -5,10 +5,78 @@
  */
 package Controlador;
 
+import Modelo.Cliente;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author alumnodaw
  */
 public class IODatos {
     
-}
+    public void guardarClientes(ArrayList<Cliente> vClientes) throws IOException{
+        ArrayList Clientes = new ArrayList();
+        String rutaFichero = "Clientes.dat";
+        File f = new File(rutaFichero);
+        if (!f.exists()) {
+            try {
+                f.createNewFile();
+            } catch (IOException ex) {
+                Logger.getLogger(IODatos.class.getName()).log(Level.SEVERE, null, ex);
+            }
+         
+            
+        }
+        
+               try (FileOutputStream fo = new FileOutputStream(f);
+            ObjectOutputStream escribir = new ObjectOutputStream(fo);) {
+
+            escribir.writeObject(vClientes);
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(IODatos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+    
+    public static ArrayList<Cliente> cargarDatos() throws IOException{
+        ArrayList<Cliente> vClientes = new ArrayList();
+        String rutaFichero = "Clientes.dat";
+        File f = new File(rutaFichero);
+        if (!f.exists()) {
+            try {
+                f.createNewFile();
+            } catch (IOException ex) {
+                Logger.getLogger(IODatos.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }
+            try(FileInputStream fi = new FileInputStream(f);
+                    ObjectInputStream leer = new ObjectInputStream(fi);){
+                
+                vClientes = (ArrayList<Cliente>) leer.readObject(); 
+                } 
+            catch (FileNotFoundException ex) { Logger.getLogger(IODatos.class.getName()).log(Level.SEVERE, null, ex); } 
+            catch (IOException ex) { Logger.getLogger(IODatos.class.getName()).log(Level.SEVERE, null, ex); } 
+            catch (ClassNotFoundException ex) { Logger.getLogger(IODatos.class.getName()).log(Level.SEVERE, null, ex); } 
+            
+            return vClientes; 
+    
+        }
+        
+    }
+        
+
+    
+    
+    
+
--- src/Modelo/Cliente.java
+++ src/Modelo/Cliente.java
@@ -21,6 +21,61 @@ public class Cliente {
         this.sexo = sexo;
         this.edad = edad;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public String getEstadocivil() {
+        return estadocivil;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public void setEstadocivil(String estadocivil) {
+        this.estadocivil = estadocivil;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nombre=" + nombre + ", apellido1=" + apellido1 + ", apellido2=" + apellido2 + ", estadocivil=" + estadocivil + ", sexo=" + sexo + ", edad=" + edad + '}';
+    }
     
 }
 
